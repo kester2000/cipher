922cef208123e9b4e944bf101c73c77697b8b618
kester <kester@lindeni.com>
2023-05-10 10:22:41 +0800

    add inside in test poetry

--- test_poetry.py
+++ test_poetry.py
@@ -1,12 +1,17 @@
 import pinyin
 
+
+def inside(a, b, mp):
+    return a in mp and b in mp and any(mp[a] == mp[b][i:i+len(mp[a])] for i in range(len(mp[b])-len(mp[a])+1))
+
+
 with open('poetrys.txt', 'r', encoding='utf-8') as f:
     poetrys = f.read().split()
 with open('chinexe_split.txt', 'r', encoding='utf-8') as f:
     chinexe_split = eval(f.read())
-# l = [k for k in d if '亻化右' in ''.join(d[k])]
+
 for p in poetrys:
     if len(p) == 7 and p[6] == '风' \
-            and pinyin.get(p, ' ', 'strip').split()[3] == 'fan' \
-            and p[4] in chinexe_split and '亻化右' in ''.join(chinexe_split[p[4]]):
+            and pinyin.get(p[3], format='strip') == 'fan' \
+            and inside('化', p[4], chinexe_split):
         print(p)
