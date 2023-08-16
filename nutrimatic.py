57575a4503f16499cb9f9152afd2fd271770c03d
kester <kester@lindeni.com>
2023-08-16 19:38:40 +0800

    change the directory structure

--- nutrimatic.py
+++
@@ -1,8 +0,0 @@
-import requests
-import re
-
-q = 'hello'
-x = requests.get('https://nutrimatic.org/?q={}'.format(q))
-values = re.findall(
-    '<span style=\'font-size: \d*\.\d*em\'>(.*)</span><br>', x.text)
-print(values)
---
+++ other.md
@@ -0,0 +1,4 @@
+搜词网址         nutrimatic https://nutrimatic.org/
+数列找规律       OEIS https://oeis.org/
+康威生命游戏     golly
+智能翻译        焖肉面 https://philippica.github.io/cipher_machine/
